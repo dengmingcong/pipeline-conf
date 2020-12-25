f6cdcebc7e3450047f2b219a7c21e4d7503a5575
Raigor Deng <raigordeng@etekcity.com.cn>
2020-12-25 09:49:52 +0800

    json assert 增加预期和实际

--- bin/jsonAssertion.groovy
+++ bin/jsonAssertion.groovy
@@ -1,16 +1,36 @@
+#!/bin/bash/groovy
+
 import groovy.json.JsonOutput
 import static com.jayway.jsonpath.JsonPath.parse
 
+// Get response
 String responseText = prev.getResponseDataAsString();
-log.info("Response text: " + responseText)
+log.info("Response: " + responseText)
 
+// Get JSON path and expected string from parameters
+log.info("Parameters: " + Parameters)
 def jsonPath = args[0];
+log.info("JSON Path: ${jsonPath}")
 def expectedJsonString = Parameters.replace(jsonPath, "");
 log.info("Expected JSON string: " + expectedJsonString);
 
+// Get actual string by JSON path
 def jsonObject = parse(responseText);
 def actualJsonString = jsonObject.read(jsonPath);
-actualJsonString = JsonOutput.toJson(actualJsonString)  
+actualJsonString = JsonOutput.toJson(actualJsonString);  
 log.info("Actual JSON string: " + actualJsonString);
 
-org.skyscreamer.jsonassert.JSONAssert.assertEquals(expectedJsonString, actualJsonString, false)
+responseHistory = """
+
+* JSON Path
+${jsonPath}
+
+* Expected
+${expectedJsonString}
+
+* Got
+${actualJsonString}
+
+"""
+
+org.skyscreamer.jsonassert.JSONAssert.assertEquals(responseHistory, expectedJsonString, actualJsonString, false);
