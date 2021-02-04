c919bb265e36b3473a8f920077a888b524c9d2ce
yukin <yukincao@etekcity.com.cn>
2021-02-04 17:24:17 +0800

    优化jmeter冒烟脚本，推送监控的逻辑，避免多次添加JSR223Listener的监控

--- bin/http_requests_add_JSR223Listener.py
+++ bin/http_requests_add_JSR223Listener.py
@@ -7,12 +7,20 @@ dst = sys.argv[2]
 
 def http_request_add_JSR223Listener():
     ltree = LET.parse(src)
-
+    jmeter_test_plan = ltree.getroot()
+    
+    if (jmeter_test_plan.get("monitored")):
+        return
+    else:
+        jmeter_test_plan.set("monitored", "true") 
+        
     hash_trees = ltree.xpath(".//HTTPSamplerProxy/following-sibling::hashTree[1]")
+    
     for hash_tree in hash_trees:
         # fist http requests is preheat interface, no need to upload monitoring
         if (hash_trees.index(hash_tree) == 0):
             continue
+            
         # add one sub-elements
         jsr223_tree = LET.SubElement(hash_tree, "JSR223Listener")
         
@@ -31,8 +39,9 @@ def http_request_add_JSR223Listener():
         
         cache_key = LET.SubElement(jsr223_tree, "stringProp", attrib={"name": "cacheKey"})
         cache_key.text = "true"
-        
+
     ltree.write(dst,encoding='utf-8', xml_declaration=True, pretty_print=True)
+    
 
 if __name__ == "__main__":
     http_request_add_JSR223Listener()
