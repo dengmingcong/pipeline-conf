96d7f031a606043999d553aac28d4239da24392c
Raigor Deng <raigordeng@etekcity.com.cn>
2021-04-09 09:31:41 +0800

    refactor: 除了将获取的用户 token 和 id 保存到 property 外，将组装后的通用请求参数也保存到 property 中

--- bin/account.groovy
+++ bin/account.groovy
@@ -1,31 +1,118 @@
+#!/bin/bash/groovy
+
+/*
+** 将接口通用参数组装成字符串，并保存到特定的 property（property 名字和指定的用户有关）中。
+** 
+** 1. 用户登录成功后，获取用户的 accountID 和 token
+** 2. 根据第一个参数的值，决定是为运营管理平台组装（setOps），还是为 VeSync 用户组装（setVeSync）
+** 3. 根据第二个参数的值，决定最终的 property 名字。注意
+**      * 系统属性需要以 vesync.accountxx.appVersion 的形式命名
+**      * 第二个参数（args[1]）必须是 accountxx
+*/
+
 import groovy.json.JsonOutput
 import static com.jayway.jsonpath.JsonPath.parse
 
+/*
+** Save common parameters for operation management system login session to specific property after login successfully.
+** 
+** property value:
+**   "accountID": "${accountID}",
+**   "token": "${token}",
+**   "timeZone": "America/New_York",
+**   "acceptLanguage": "en",
+**   "osInfo": "osInfo",
+**   "clientInfo": "clientInfo",
+**   "clientType": "clientType",
+**   "clientVersion": "clientVersion",
+**   "terminalId": "terminalId",
+**   "debugMode": true,
+*/
+def setOpsLoginSessionCommonParams(propertyName) {
+	String commonParams = """\
+        "accountID": "${accountID}",
+        "token": "${token}",
+        "timeZone": "America/New_York",
+        "acceptLanguage": "en",
+        "osInfo": "osInfo",
+        "clientInfo": "clientInfo",
+        "clientType": "clientType",
+        "clientVersion": "clientVersion",
+        "terminalId": "terminalId",
+        "debugMode": true,
+	""".stripIndent();
+	
+	props.put(propertyName, commonParams);
+	log.info("name: ${propertyName}, value: ${commonParams}")
+}
+
+/*
+** Save common parameters for vesync login session to specific property after login successfully.
+** 
+** Note:
+**   Variables appVersion, phoneOS, timeZone must be set in the scope.
+**
+** property value:
+**   "acceptLanguage": "en",
+**   "appVersion": "${appVersion}",
+**   "phoneBrand": "TestBrand",
+**   "phoneOS": "${phoneOS}",
+**   "timeZone": "${timeZone}",
+**   "token": "${token}",
+**   "accountID": "${accountID}",
+**   "debugMode": true,
+*/
+def setVeSyncLoginSessionCommonParams(propertyName, id) {
+	String appVersionPropName = "vesync.${id}.appVersion";
+	String phoneOSPropName = "vesync.${id}.phoneOS";
+	String timeZonePropName = "vesync.${id}.timeZone"
+	
+	String appVersion = props.get(appVersionPropName);
+	String phoneOS = props.get(phoneOSPropName);
+	String timeZone = props.get(timeZonePropName);
+	
+	String commonParams = """\
+		"acceptLanguage": "en",
+		"appVersion": "${appVersion}",
+		"phoneBrand": "TestBrand",
+		"phoneOS": "${phoneOS}",
+		"timeZone": "${timeZone}",
+		"token": "${token}",
+		"accountID": "${accountID}",
+		"debugMode": true,
+	""".stripIndent();
+	
+	props.put(propertyName, commonParams);
+	log.info("name: ${propertyName}, value: ${commonParams}")
+}
+
 String accountIDJSONPath = '$.result.accountID';
 String tokenJSONPath = '$.result.token';
-String accoutIDPropertyName = "account.id";
-String tokenPropertyName = "account.token";
-String accountIDVarName = "accountID";
-String tokenVarName = "token";
 
-if (Parameters == "get") {
-	// Get response
-	String responseText = prev.getResponseDataAsString();
-	log.info("Response: " + responseText)
+// Get login response
+String responseText = prev.getResponseDataAsString();
+log.info("Response: " + responseText)
 
-	// Get accountID and token
-	def jsonObject = parse(responseText);
-	def accountID = jsonObject.read(accountIDJSONPath);
-	def token = jsonObject.read(tokenJSONPath);
-	log.info("accountID: ${accountID}, token: ${token}")
+// Get accountID and token
+jsonObject = parse(responseText);
+accountID = jsonObject.read(accountIDJSONPath);
+token = jsonObject.read(tokenJSONPath);
+log.info("accountID: ${accountID}, token: ${token}");
 
-	// Set properties
-	props.put(accoutIDPropertyName, accountID);
-	props.put(tokenPropertyName, token);
-} else if (Parameters == "set") {
-	def accountID = props.get(accoutIDPropertyName);
-	def token = props.get(tokenPropertyName);
-	
-	vars.put(accountIDVarName, accountID)
-	vars.put(tokenVarName, token)
+accountCode = args[1];
+log.info("account code (second parameter assigned): ${accountCode}");
+
+if (args[0] == "setOps") {
+	String propertyName = "ops.account.${accountCode}.login-session.common-parameters";
+	setOpsLoginSessionCommonParams(propertyName);
+} else if (args[0] == "setVeSync") {
+	String propertyName = "vesync.${accountCode}.login-session.common-parameters";
+	setVeSyncLoginSessionCommonParams(propertyName, accountCode);
 }
+
+String accountIDPropName = "${accountCode}.accountID"
+String tokenPropName = "${accountCode}.token"
+props.put(accountIDPropName, accountID)
+props.put(tokenPropName, token)
+log.info("accountID property, name: ${accountIDPropName}, value: ${accountID}")
+log.info("token property, name: ${tokenPropName}, value: ${token}")
