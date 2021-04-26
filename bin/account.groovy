7bec217b5cf7928cda2eee2ddab06ee7a97d6b20
Raigor Deng <raigordeng@etekcity.com.cn>
2021-04-26 14:40:22 +0800

    refactor: 【account.groovy】【增加保存商城用户的功能】

--- bin/account.groovy
+++ bin/account.groovy
@@ -4,10 +4,14 @@
 ** 将接口通用参数组装成字符串，并保存到特定的 property（property 名字和指定的用户有关）中。
 ** 
 ** 1. 用户登录成功后，获取用户的 accountID 和 token
-** 2. 根据第一个参数的值，决定是为运营管理平台组装（setOps），还是为 VeSync 用户组装（setVeSync）
-** 3. 根据第二个参数的值，决定最终的 property 名字。注意
-**      * 系统属性需要以 vesync.accountxx.appVersion 的形式命名
-**      * 第二个参数（args[1]）必须是 accountxx
+** 2. 根据第一个参数的值，决定是为哪个用户组装
+**      - setOps：运营管理平台用户
+**      - setOpsMall：商城管理用户
+**      - setVeSync：VeSync 用户
+**      - setVeSyncMall：VeSync 商城用户
+** 3. 第二个参数为用户的代号，最终的 property 名字和此代号有关
+** 4. 如果第一个参数是 setVeSync 或者 setVeSyncMall，需要指定第三个参数，取属性名的前缀
+**    如：若属性名为 mall.client.account01.email，第三个参数赋值为 mall.client.
 */
 
 import groovy.json.JsonOutput
@@ -15,18 +19,6 @@ import static com.jayway.jsonpath.JsonPath.parse
 
 /*
 ** Save common parameters for operation management system login session to specific property after login successfully.
-** 
-** property value:
-**   "accountID": "${accountID}",
-**   "token": "${token}",
-**   "timeZone": "America/New_York",
-**   "acceptLanguage": "en",
-**   "osInfo": "osInfo",
-**   "clientInfo": "clientInfo",
-**   "clientType": "clientType",
-**   "clientVersion": "clientVersion",
-**   "terminalId": "terminalId",
-**   "debugMode": true,
 */
 def setOpsLoginSessionCommonParams(propertyName) {
 	String commonParams = """\
@@ -47,20 +39,47 @@ def setOpsLoginSessionCommonParams(propertyName) {
 }
 
 /*
+** Save common parameters for operation mall management system login session to specific property after login successfully.
+*/
+def setOpsMallLoginSessionCommonParams(propertyName, propertyPrefix) {
+	String timeZonePropName = "${propertyPrefix}timeZone";
+	String acceptLanguagePropName = "${propertyPrefix}acceptLanguage";
+	String osInfoPropName = "${propertyPrefix}osInfo"
+	String clientInfoPropName = "${propertyPrefix}clientInfo"
+	String clientTypePropName = "${propertyPrefix}clientType"
+	String clientVersionPropName = "${propertyPrefix}clientVersion"
+	
+	String timeZone = props.get(timeZonePropName);
+	String acceptLanguage = props.get(acceptLanguagePropName);
+	String osInfo = props.get(osInfoPropName);
+	String clientInfo = props.get(clientInfoPropName);
+	String clientType = props.get(clientTypePropName);
+	String clientVersion = props.get(clientVersionPropName);
+	
+	String terminalId = vars.get("terminalId")
+
+	String commonParams = """\
+        "accountID": "${accountID}",
+        "token": "YzkyZDhjYTUwYmRiZTI4YjU4MjAzZWVlY2JmMjMwZjBhMDYyMzAwMGRiNTViZmY1ZjAxNmY0N2U1MzY5OTExMA==",
+        "timeZone": "${timeZone}",
+        "acceptLanguage": "${acceptLanguage}",
+        "osInfo": "${osInfo}",
+        "clientInfo": "${clientInfo}",
+        "clientType": "${clientType}",
+        "clientVersion": "${clientVersion}",
+        "terminalId": "${terminalId}",
+        "debugMode": true,
+	""".stripIndent();
+	
+	props.put(propertyName, commonParams);
+	log.info("name: ${propertyName}, value: ${commonParams}")
+}
+
+/*
 ** Save common parameters for vesync login session to specific property after login successfully.
 ** 
 ** Note:
 **   Variables appVersion, phoneOS, timeZone must be set in the scope.
-**
-** property value:
-**   "acceptLanguage": "en",
-**   "appVersion": "${appVersion}",
-**   "phoneBrand": "TestBrand",
-**   "phoneOS": "${phoneOS}",
-**   "timeZone": "${timeZone}",
-**   "token": "${token}",
-**   "accountID": "${accountID}",
-**   "debugMode": true,
 */
 def setVeSyncLoginSessionCommonParams(propertyName, id, propertyPrefix) {
 	String appVersionPropName = "${propertyPrefix}${id}.appVersion";
@@ -86,6 +105,39 @@ def setVeSyncLoginSessionCommonParams(propertyName, id, propertyPrefix) {
 	log.info("name: ${propertyName}, value: ${commonParams}")
 }
 
+/*
+** Save common parameters for vesync mall login session to specific property after login successfully.
+** 
+** Note:
+**   Variables appVersion, phoneOS, timeZone must be set in the scope.
+*/
+def setVeSyncMallLoginSessionCommonParams(propertyName, id, propertyPrefix) {
+	String appVersionPropName = "${propertyPrefix}${id}.appVersion";
+	String phoneOSPropName = "${propertyPrefix}${id}.phoneOS";
+	String timeZonePropName = "${propertyPrefix}${id}.timeZone"
+	
+	String appVersion = props.get(appVersionPropName);
+	String phoneOS = props.get(phoneOSPropName);
+	String timeZone = props.get(timeZonePropName);
+	String uuid = UUID.randomUUID().toString()
+	
+	String commonParams = """\
+		"acceptLanguage": "en",
+		"clientVersion": "${appVersion}",
+		"clientType": "vesyncApp",
+		"clientInfo": "${phoneOS}",
+		"osInfo": "${phoneOS}",
+		"timeZone": "${timeZone}",
+		"token": "${token}",
+		"accountID": "${accountID}",
+		"debugMode": true,
+		"terminalId": "${uuid}",
+	""".stripIndent();
+	
+	props.put(propertyName, commonParams);
+	log.info("name: ${propertyName}, value: ${commonParams}")
+}
+
 String accountIDJSONPath = '$.result.accountID';
 String tokenJSONPath = '$.result.token';
 
@@ -105,6 +157,14 @@ log.info("account code (second parameter assigned): ${accountCode}");
 if (args[0] == "setOps") {
 	String propertyName = "ops.account.${accountCode}.login-session.common-parameters";
 	setOpsLoginSessionCommonParams(propertyName);
+} else if (args[0] == "setOpsMall") {
+	String propertyPrefix = "mall.management.common-params."
+	if (args.size() == 3) {
+		propertyPrefix = args[2]
+	}
+	
+	String propertyName = "ops.mall.account.${accountCode}.login-session.common-parameters";
+	setOpsMallLoginSessionCommonParams(propertyName, propertyPrefix);
 } else if (args[0] == "setVeSync") {
 	String propertyPrefix = "vesync."
 	if (args.size() == 3) {
@@ -113,6 +173,14 @@ if (args[0] == "setOps") {
 	
 	String propertyName = "vesync.${accountCode}.login-session.common-parameters";
 	setVeSyncLoginSessionCommonParams(propertyName, accountCode, propertyPrefix);
+} else if (args[0] == "setVeSyncMall") {
+	String propertyPrefix = "vesync."
+	if (args.size() == 3) {
+		propertyPrefix = args[2]
+	}
+	
+	String propertyName = "vesync.mall.${accountCode}.login-session.common-parameters";
+	setVeSyncMallLoginSessionCommonParams(propertyName, accountCode, propertyPrefix);
 }
 
 String accountIDPropName = "${accountCode}.accountID"
