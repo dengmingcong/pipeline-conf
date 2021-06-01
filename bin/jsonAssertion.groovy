1f631c6b5e56f87f4b7bc87f37c5487e4b8f8cfe
Raigor Deng <raigordeng@etekcity.com.cn>
2021-06-01 18:52:28 +0800

    refactor: jsonAssertion.groovy 新增支持 json path 中字符串包含空格

--- bin/jsonAssertion.groovy
+++ bin/jsonAssertion.groovy
@@ -4,6 +4,8 @@ import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.jayway.jsonpath.JsonPath
 
+String delimiter = "__@@__"
+
 // Get response
 String responseText = prev.getResponseDataAsString();
 log.info("raw response: ${responseText}")
@@ -14,9 +16,19 @@ log.info("response parsed: ${responseText}")
 
 // Get JSON path and expected string from parameters
 log.info("Parameters: " + Parameters)
-def jsonPath = args[0];
+String jsonPath = ""
+String expectedJsonString = ""
+
+if (Parameters.contains(delimiter)) {
+	splited = Parameters.split(delimiter)
+	jsonPath = splited[0]
+	expectedJsonString = splited[1]
+} else {
+	jsonPath = args[0];
+	expectedJsonString = Parameters.replace(jsonPath, "");
+}
+
 log.info("JSON Path: ${jsonPath}")
-def expectedJsonString = Parameters.replace(jsonPath, "");
 log.info("Expected JSON string: " + expectedJsonString);
 
 // Get response string by JSON path
